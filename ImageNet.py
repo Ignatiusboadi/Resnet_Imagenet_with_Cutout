e1bc9e3dd7495a43ba1027c9ba48d2f28d5863f9
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-06-12 22:27:16 +0000

    created an ImageNet class for loading the dataset; loaded a json file for the classes and mapped the classes to an index

--- ImageNet.py
+++ ImageNet.py
@@ -1,3 +1,16 @@
+import json
 import os
 from torch.utils.data import Dataset
+
+
+class ImageNet(Dataset):
+    def __init__(self, root, split, transform=None):
+        super().__init__()
+        self.samples = []  # list of images
+        self.targets = []  # list of labels of images
+        self.class_indices = {}  # dictionary mapping class_ids and a class index
+        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as json_file:
+            class_index_file = json.load(json_file)
+            for class_id, class_items in class_index_file.items():
+                self.class_indices[class_items[0]] = int(class_id)
 
