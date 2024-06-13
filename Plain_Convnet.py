0c786566048995c2fa0f213f8aa30fc8ad407bb3
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-06-13 00:04:34 +0000

    added PlainConvNet class for the network

--- Plain_Convnet.py
+++ Plain_Convnet.py
@@ -28,3 +28,40 @@ class BasicBlock(nn.Module):
         return out
 
 
+class PlainConvNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        super().__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(64, 2, stride=1)
+        self.layer2 = self._make_layer(128, 2, stride=2)
+        self.layer3 = self._make_layer(256, 2, stride=2)
+        self.layer4 = self._make_layer(512, 2, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def _make_layer(self, out_channels, blocks, stride):
+        layers = [BasicBlock(self.in_channels, out_channels, stride)]
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(BasicBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        x = self.log_softmax(x)
+        return x
