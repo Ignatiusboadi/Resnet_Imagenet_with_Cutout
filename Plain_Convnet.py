b794e3e7d18d457765f270b544fb4a8834cd58c5
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-06-13 00:00:08 +0000

    added BasicBlock class to define a block in the network as spelt out in the ResNet paper

---
+++ Plain_Convnet.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+import torch
+
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+from ImageNet import ImageNet
+from tqdm import tqdm
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        return out
+
+
