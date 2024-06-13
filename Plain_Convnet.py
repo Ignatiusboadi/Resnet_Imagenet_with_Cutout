b76ba6e7240af24dd3be3914137a13bba4526ff5
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-06-13 00:17:54 +0000

    added dataloader codes for training and validation datasets

--- Plain_Convnet.py
+++ Plain_Convnet.py
@@ -71,4 +71,42 @@ class PlainConvNet(nn.Module):
 root = "/Users/ignatiusboadi/Documents/AMMI/Computer Vision/ImageNet and Resnet18/"
 
 # Load dataset
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
 
+train_transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+
+train_dataset = ImageNet(root, "train", train_transform)
+train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=64,
+            num_workers=8,
+            shuffle=False,
+            drop_last=False,
+            pin_memory=True
+        )
+
+val_transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+val_dataset = ImageNet(root, "val", val_transform)
+val_dataloader = DataLoader(
+            val_dataset,
+            batch_size=64,
+            num_workers=8,
+            shuffle=False,
+            drop_last=False,
+            pin_memory=True
+        )
